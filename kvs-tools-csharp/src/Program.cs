b5afba3447a06b299644ba1a651a33b09bfd2b79
Attacktive <66462458+Attacktive@users.noreply.github.com>
2022-05-30 18:10:49 +0900

    Implement command line arguments parsing (#1)

---
+++ kvs-tools-csharp/src/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using kvs_tools_csharp.util;
+
+namespace kvs_tools_csharp
+{
+	internal static class Program
+	{
+		private static int Main(params string[] args)
+		{
+			var (command, pathToFile) = CommandLineUtils.Parse(args);
+			if (command == Command.None)
+			{
+				Console.WriteLine(
+					@"Usage:
+extract ./bgm.ktsl2stbin
+archive ./directory/to/source/files"
+				);
+
+				return 1;
+			}
+
+			// TODO
+
+			return 0;
+		}
+	}
+}
