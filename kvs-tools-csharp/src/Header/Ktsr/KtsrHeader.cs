f4f05d1f9bfc4cc6eb2794f11cc07844455d3f6c
Attacktive <66462458+Attacktive@users.noreply.github.com>
2022-06-29 00:13:09 +0900

    Fix multiple classes within single file (#13)
    It tries to fix https://github.com/Attacktive/kvs-tools-csharp/issues/12

--- kvs-tools-csharp/src/Header/Ktsr/KtsrHeader.cs
+++ kvs-tools-csharp/src/Header/Ktsr/KtsrHeader.cs
@@ -23,7 +23,7 @@ namespace KvsTools.Header.Ktsr
 
 		public uint FileSize { get; }
 
-		public Game Game { get; }
+		public GameInfo Game { get; }
 
 		public byte[] Bytes =>
 			new List<byte>()
@@ -39,69 +39,69 @@ namespace KvsTools.Header.Ktsr
 				.Concat(Game.Entries)
 				.ToArray();
 
-		public KtsrHeader(string signature, byte[] chunkType, byte version, byte platform, uint fileSize, Game game)
+		public KtsrHeader(string signature, byte[] chunkType, byte version, byte platform, uint fileSize, GameInfo gameInfo)
 		{
 			Signature = signature;
 			ChunkType = chunkType;
 			Version = version;
 			Platform = platform;
 			FileSize = fileSize;
-			Game = game;
+			Game = gameInfo;
 		}
 
 		public override string ToString()
 		{
 			return $"Signature: {Signature}, ChunkType: {ChunkType.ToHexString()}, Version: {Version}, Platform: {Platform}, FileSize: {FileSize}, GameId: {Game.Id.ToHexString()}, GameEntries: {Game.Entries.ToHexString()}";
 		}
-	}
 
-	public class Game
-	{
-		public static readonly Game AttackOnTitan2 = new Game
-		(
-			"Attack on Titan 2",
-			new byte[] { 0x09, 0xd4, 0xf4, 0x15 },
-			new byte[]
+		public class GameInfo
+		{
+			public static readonly GameInfo AttackOnTitan2 = new GameInfo
+			(
+				"Attack on Titan 2",
+				new byte[] { 0x09, 0xd4, 0xf4, 0x15 },
+				new byte[]
+				{
+					0x09, 0xd4, 0xf4, 0x15,
+					0x20, 0xe9, 0x88, 0x00,
+					0xca, 0xab, 0xa8, 0xa9,
+					0x20, 0x00, 0x00, 0x00
+				}
+			);
+
+			public static readonly GameInfo AtelierRyza1 = new GameInfo
+			(
+				"Atelier Ryza 1",
+				new byte[] { 0x4e, 0xc5, 0xe8, 0xc5 },
+				new byte[]
+				{
+					0x09, 0xd4, 0xf4, 0x15,
+					0xf0, 0x2c, 0x1b, 0x00,
+					0x00, 0x70, 0x4c, 0x41,
+					0x20, 0x00, 0x00, 0x00
+				}
+			);
+
+			private static readonly List<GameInfo> List = new List<GameInfo>
 			{
-				0x09, 0xd4, 0xf4, 0x15,
-				0x20, 0xe9, 0x88, 0x00,
-				0xca, 0xab, 0xa8, 0xa9,
-				0x20, 0x00, 0x00, 0x00
-			}
-		);
+				AttackOnTitan2, AtelierRyza1
+			};
 
-		public static readonly Game AtelierRyza1 = new Game
-		(
-			"Atelier Ryza 1",
-			new byte[] { 0x4e, 0xc5, 0xe8, 0xc5 },
-			new byte[]
+			public string Name { get; }
+			public byte[] Id { get; }
+			public byte[] Entries { get; }
+
+			private GameInfo(string name, byte[] id, byte[] entries)
 			{
-				0x09, 0xd4, 0xf4, 0x15,
-				0xf0, 0x2c, 0x1b, 0x00,
-				0x00, 0x70, 0x4c, 0x41,
-				0x20, 0x00, 0x00, 0x00
+				Name = name;
+				Id = id;
+				Entries = entries;
 			}
-		);
 
-		private static readonly List<Game> List = new List<Game>
-		{
-			AttackOnTitan2, AtelierRyza1
-		};
-
-		public string Name { get;  }
-		public byte[] Id { get; }
-		public byte[] Entries { get; }
-
-		private Game(string name, byte[] id, byte[] entries)
-		{
-			Name = name;
-			Id = id;
-			Entries = entries;
-		}
-
-		public static Game? ById(byte[] id)
-		{
-			return List.FirstOrDefault(game => game.Id.SequenceEqual(id));
+			public static GameInfo? ById(byte[] id)
+			{
+				return List.FirstOrDefault(game => game.Id.SequenceEqual(id));
+			}
 		}
 	}
 }
